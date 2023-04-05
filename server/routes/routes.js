7e96b4ae9f896026c74b92bf13c17871c8497a94
molaraiche <molaraiche@molaraiche.com>
2023-04-05 05:07:54 +0000

    updates

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,21 @@
 const router = require("express").Router();
+const multer = require("multer");
+const multerConfig = multer.diskStorage({
+  destination: (err, file, cb) => cb(null, "public"),
+  filename: (err, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
+});
+const uploadSet = multer({ storage: multerConfig });
+const upload = uploadSet.single("image");
+
 const {
-  getCards,
   getBlogs,
   setBlog,
   updateBlog,
   deleteBlog,
 } = require("../controllers/Blogs");
-
+router.post("/upload", upload, (req, res) => {
+  res.json({ msg: req.file.path });
+});
 router.get("/", getBlogs);
 router.post("/create", setBlog);
 router.put("/all/:id", updateBlog);
